ca2b9a67fb2dcb3fc1a3da7f095d845f6e0c98fa
Sakibei_laptop <s1152855@gmail.com>
2023-08-09 16:47:26 +0800

    json資料傳入及非同步功能撰寫

---
+++ src/main/java/com/benny/controller/ProfilesController.java
@@ -0,0 +1,26 @@
+package com.benny.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ProfilesController {
+
+    @PostMapping("/profiles.controller")
+    @ResponseBody
+    public String processAction(@RequestParam("userName") String userName,
+                                @RequestParam("userAddress") String userAddress,
+                                @RequestParam("userPhone") String userPhone){
+        System.out.println("message: " + userName + " " + userAddress + " " + userPhone);
+        return "message: " + userName + " " + userAddress + " " + userPhone;
+    }
+
+    @PostMapping("/profiles2.controller")
+    @ResponseBody
+    public String processAction2(@RequestParam("userName") String userName){
+        return "message: " + userName;
+    }
+
+}
